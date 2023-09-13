1aff336dd6855f170438c6123fdd8fc3cba6f525
natalia <lnataliaperri@gmail.com>
2023-09-13 14:04:39 -0300

    feat: get question by slug use case

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
+import { Slug } from "../../enterprise/entities/value-objects/slug";
+import { Question } from "../../enterprise/entities/question";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: GetQuestionBySlugUseCase;
+describe("Get Question By Slug", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
+  });
+  it("should be able to receive a question by slug", async () => {
+    const newQuestion = Question.create({
+      title: "Example question",
+      slug: Slug.create("example-question"),
+      authorId: new UniqueEntityID(),
+      content: "Example content",
+    });
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    const { question } = await sut.execute({
+      slug: "example-question",
+    });
+
+    expect(question.id).toBeTruthy();
+    expect(question.title).toEqual(newQuestion.title);
+  });
+});
