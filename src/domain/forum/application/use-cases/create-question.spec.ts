29d28c914eddd4fdf97dc7b9597f38466d0a9589
natalia <lnataliaperri@gmail.com>
2023-09-13 13:39:26 -0300

    ref: created in-memory repositories to suits tests and refactored tests with it

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,22 +1,21 @@
-import { Question } from "../../enterprise/entities/question";
-import { QuestionsRepository } from "../repositories/questions-repository";
-import { AnswerQuestionUseCase } from "./answer-question";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 import { CreateQuestionUseCase } from "./create-question";
 
-const fakeQuestionsRepository: QuestionsRepository = {
-  create: async function (question: Question) {
-    return;
-  },
-};
-
-test("create a question", async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository);
-
-  const { question } = await createQuestion.execute({
-    authorId: "1",
-    title: "Nova pergunta",
-    content: "Conteúdo da pergunta",
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: CreateQuestionUseCase;
+describe("Create Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository);
   });
+  it("should be able to create a question", async () => {
+    const { question } = await sut.execute({
+      authorId: "1",
+      title: "Nova pergunta",
+      content: "Conteúdo da pergunta",
+    });
 
-  expect(question.id).toBeTruthy();
+    expect(question.id).toBeTruthy();
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id);
+  });
 });
